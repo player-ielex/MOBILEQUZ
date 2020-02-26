eb1512c28431302db3b4f3f0279fe1983b38c395
PaulIelex <paulIelex20@gmail.com>
2020-02-26 18:29:15 +0900

    First Commit of the App

---
+++ app/src/main/java/com/teamtreehouse/mobilequz/SplashScreenActivity.java
@@ -0,0 +1,64 @@
+package com.teamtreehouse.mobilequz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    public volatile boolean interrupt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread nest = new Thread() {
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (!interrupt) {
+                        Intent intent = new Intent(SplashScreenActivity.this, SplashScreenActivity.class);
+                        startActivity(intent);
+                    }
+                }
+            }
+        };
+        nest.start();
+    }
+
+
+    @Override
+    public void onUserLeaveHint() {
+        super.onUserLeaveHint();
+        finish();
+        interrupt = true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+        interrupt = true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        finish();
+    }
+
+
+}
+
+
+
